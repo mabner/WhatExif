e06427d529d5efb87b2187ee980a9d3a4983bb2e
Marcos Leite <m.abner@live.co.uk>
2022-08-31 20:19:24 -0300

    alters exif.js to non minimized version

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import '../scss/styles.scss';
 
 import './vendor/modernizr-3.11.2.min.js';
-import './vendor/exif-js.min.js';
+import './vendor/exif.js';
 import './plugins.js';
 import './main.js';
 

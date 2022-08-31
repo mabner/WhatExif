55e85132cef7ee236f34afe0bd903a19c4b655b1
Marcos Leite <m.abner@live.co.uk>
2022-08-31 20:57:31 -0300

    exif reading test

--- src/js/main.js
+++ src/js/main.js
@@ -80,3 +80,23 @@ let handleFiles = (files) => {
 	console.log(`Number of images: ${imageAsset.size}`);
 	// console.log(imageName)
 };
+
+
+window.onload = getExif;
+
+function getExif() {
+	var img1 = document.getElementById('newImage1');
+	EXIF.getData(img1, function () {
+		var make = EXIF.getTag(this, 'Make');
+		var model = EXIF.getTag(this, 'Model');
+		var makeAndModel = document.getElementById('makeAndModel');
+		makeAndModel.innerHTML = `${make} ${model}`;
+	});
+
+	var img2 = document.getElementById('newImage2');
+	EXIF.getData(img2, function () {
+		var allMetaData = EXIF.getAllTags(this);
+		var allMetaDataSpan = document.getElementById('allMetaDataSpan');
+		allMetaDataSpan.innerHTML = JSON.stringify(allMetaData, null, '\t');
+	});
+}
